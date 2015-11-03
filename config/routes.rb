3d78afbcc5f51aa534b8ed4db2b26201175aa779
Ben Ricker <bgricker@gmail.com>
2015-11-03 08:31:28 -0500

    Photo shouts, user following

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    post 'follow' => 'following_relationships#create'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
--- notes.md
+++ notes.md
@@ -2,4 +2,4 @@ helpers:
   - module mixed in with view class, default mixes in helpers in app/helpers
   - all methods in view class available in templates
 
-
+adding relations in the model, must specify class if it differs from a model name
